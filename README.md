e44273f3546987db00a662a358b749170314fb30
miche <michelangelofantini.mail@gmail.com>
2025-03-06 14:45:07 -0500

    task2 simulation

--- README.md
+++ README.md
@@ -14,3 +14,29 @@ Go to the `nodes` directory of the relevant package (for example, `snowball/lab2
 ```
 PYTHONPATH=$PWD pytest
 ```
+
+## LAB 3
+
+### TASK 2 - Simulation
+
+#### Simulation using the rosbag
+3 terminals
+
+run roscore
+```
+source devel/setup.bash
+roscore
+```
+
+run the estimator
+```
+source devel/setup.bash
+rosrun rob521_lab3 l3_estimate_robot_motion.py
+```
+
+launch rosbag
+```
+source devel/setup.bash
+roscd rob521_lab3/  
+rosbag play sample_data.bag --pause
+```
--- snowball/lab3/nodes/l3_estimate_robot_motion.py
+++ snowball/lab3/nodes/l3_estimate_robot_motion.py
@@ -8,12 +8,14 @@ import tf_conversions
 import tf2_ros
 import rosbag
 import rospkg
+from math import cos, sin
 
 # msgs
 from turtlebot3_msgs.msg import SensorState
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Pose, Twist, TransformStamped, Transform, Quaternion
 from std_msgs.msg import Empty
+
 
 from utils import convert_pose_to_tf, euler_from_ros_quat, ros_quat_from_euler
 
@@ -22,6 +24,7 @@ ENC_TICKS = 4096
 RAD_PER_TICK = 0.001533981
 WHEEL_RADIUS = .066 / 2
 BASELINE = .287 / 2
+INT32_MAX = 2**31
 
 
 class WheelOdom:
@@ -48,6 +51,10 @@ class WheelOdom:
         self.last_enc_r = None
         self.last_time = None
 
+        # estimation parameters
+        self.mu = np.array([0, 0, 0]).reshape(-1, 1) # assume zero origin
+        self.cov = np.diag([1e-3, 1e-3, 1e-3])
+
         # rosbag
         rospack = rospkg.RosPack()
         path = rospack.get_path("rob521_lab3")
@@ -59,35 +66,65 @@ class WheelOdom:
         while not rospy.is_shutdown() and (self.odom.pose.pose.position.x >= 1e-3 or self.odom.pose.pose.position.y >= 1e-3 or
                self.odom.pose.pose.orientation.z >= 1e-2):
             time.sleep(0.2)  # allow reset_pub to be ready to publish
-        print('Robot odometry reset.')
+        print('Robot o  dometry reset.')
 
         rospy.spin()
         self.bag.close()
         print("saving bag")
 
+    def A(self, theta_bar):
+        pass
+
+    def B(self, theta_bar):
+        return np.array([[cos(theta_bar), 0], [sin(theta_bar), 0], [0, 1]]) @ \
+        np.array([[WHEEL_RADIUS /2, WHEEL_RADIUS /2 ], [WHEEL_RADIUS/(2*BASELINE), -WHEEL_RADIUS/(2*BASELINE)]])
+
+    def safeDelPhi(self, a, b):
+        #Need to check if the encoder storage variable has overflowed
+        diff = np.int64(b) - np.int64(a)
+        if diff < -np.int64(INT32_MAX): #Overflowed
+            delPhi = (INT32_MAX - 1 - a) + (INT32_MAX + b) + 1
+        elif diff > np.int64(INT32_MAX) - 1: #Underflowed
+            delPhi = (INT32_MAX + a) + (INT32_MAX - 1 - b) + 1
+        else:
+            delPhi = b - a  
+        return delPhi
+
     def sensor_state_cb(self, sensor_state_msg):
         # Callback for whenever a new encoder message is published
         # set initial encoder pose
         if self.last_enc_l is None:
-            self.last_enc_l = sensor_state_msg.left_encoder
-            self.last_enc_r = sensor_state_msg.right_encoder
+            self.last_enc_l = sensor_state_msg.left_encoder #int32
+            self.last_enc_r = sensor_state_msg.right_encoder #int32
             self.last_time = sensor_state_msg.header.stamp
         else:
+            self.topic_time = sensor_state_msg.header.stamp 
             # update calculated pose and twist with new data
-            le = sensor_state_msg.left_encoder
-            re = sensor_state_msg.right_encoder
-
-            # # YOUR CODE HERE!!!
+            le = sensor_state_msg.left_encoder #int32
+            re = sensor_state_msg.right_encoder #int32
+            if self.topic_time - self.last_time > rospy.Duration(0.1):
+                print("Time difference too large: %2.3f" % (self.topic_time - self.last_time).to_sec())
+            
+            # make sure to account for tick overflow
+            del_left_encoder = self.safeDelPhi(self.last_enc_l, le)
+            del_right_encoder = self.safeDelPhi(self.last_enc_r, re)
             # Update your odom estimates with the latest encoder measurements and populate the relevant area
             # of self.pose and self.twist with estimated position, heading and velocity
+            delta_phi = np.array([RAD_PER_TICK * (del_right_encoder), RAD_PER_TICK * (del_left_encoder)]).reshape(-1, 1)
 
-            # self.pose.position.x = xx
-            # self.pose.position.y = xx
-            # self.pose.orientation = xx
+            theta_bar = self.mu[2]
+            new_mu = self.mu + self.B(theta_bar) @ delta_phi
+            mu_dot = (new_mu - self.mu)/(self.topic_time - self.last_time).to_sec()
 
-            # self.twist.linear.x = mu_dot[0].item()
-            # self.twist.linear.y = mu_dot[1].item()
-            # self.twist.angular.z = mu_dot[2].item()
+            # TODO covariance update
+
+            self.pose.position.x = new_mu[0]
+            self.pose.position.y = new_mu[1]
+            self.pose.orientation = ros_quat_from_euler([0, 0, new_mu[2]])
+
+            self.twist.linear.x = mu_dot[0].item()
+            self.twist.linear.y = mu_dot[1].item()
+            self.twist.angular.z = mu_dot[2].item()
 
             # publish the updates as a topic and in the tf tree
             current_time = rospy.Time.now()
@@ -102,14 +139,21 @@ class WheelOdom:
 
             self.bag.write('odom_est', self.wheel_odom)
 
+            # modify the previous 
+            self.mu = new_mu
+            self.last_enc_l = le
+            self.last_enc_r = re
+            self.last_time = self.topic_time
+
             # for testing against actual odom
-            # print("Wheel Odom: x: %2.3f, y: %2.3f, t: %2.3f" % (
-            #     self.pose.position.x, self.pose.position.y, mu[2].item()
-            # ))
-            # print("Turtlebot3 Odom: x: %2.3f, y: %2.3f, t: %2.3f" % (
-            #     self.odom.pose.pose.position.x, self.odom.pose.pose.position.y,
-            #     euler_from_ros_quat(self.odom.pose.pose.orientation)[2]
-            # ))
+            mu = new_mu
+            print("Wheel Odom: x: %2.3f, y: %2.3f, t: %2.3f" % (
+                self.pose.position.x, self.pose.position.y, mu[2].item()
+            ))
+            print("Turtlebot3 Odom: x: %2.3f, y: %2.3f, t: %2.3f" % (
+                self.odom.pose.pose.position.x, self.odom.pose.pose.position.y,
+                euler_from_ros_quat(self.odom.pose.pose.orientation)[2]
+            ))
 
     def odom_cb(self, odom_msg):
         # get odom from turtlebot3 packages
